2a2c76969cdbac10ada1afb4c839d5b70d405c0a
john <john@johndeimac.local>
2016-05-24 15:48:16 +0800

    初始化项目

---
+++ qianchengwuyou/qianchengwuyou/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qianchengwuyou
+//
+//  Created by john on 16/5/24.
+//  Copyright © 2016年 ckymuky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ qianchengwuyou/qianchengwuyou/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qianchengwuyou
+//
+//  Created by john on 16/5/24.
+//  Copyright © 2016年 ckymuky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
